083fc5960119a4c08e58029e46fa07c6d6448f11
Ricardo Miranda <mail@ricardoMiranda>
2020-05-02 18:12:40 +0100

    Update in test reference to correct test file name

--- src/test/scala/com/ricardomiranda/magicsquare/CoreTest.scala
+++ src/test/scala/com/ricardomiranda/magicsquare/CoreTest.scala
@@ -6,14 +6,16 @@ class CoreTest extends wordspec.AnyWordSpec with matchers.should.Matchers {
 
   "get file contents" should {
     "return None when getting file contents when the file doesn't exist" in {
-      val actual = CoreFunctions
+      val actual = Core
         .getFileContents(filePath = "non_existent_file.json")
       actual shouldEqual None
     }
 
     "return file contents when getting file contents when the file exist" in {
-      val actual = CoreFunctions
-        .getFileContents(filePath = getClass.getResource("/magic_square_empty_file.csv").getPath)
+      val actual = Core
+        .getFileContents(filePath =
+          getClass.getResource("/magic_square_empty_file.json").getPath
+        )
       actual shouldEqual Some("{}")
     }
   }
