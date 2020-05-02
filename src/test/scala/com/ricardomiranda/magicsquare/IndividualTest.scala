6e610fa47e3f2d564b612d8bcd9fa67c4e613d9e
Ricardo Miranda <mail@ricardoMiranda>
2020-05-02 22:14:25 +0100

    Test for mutation

--- src/test/scala/com/ricardomiranda/magicsquare/IndividualTest.scala
+++ src/test/scala/com/ricardomiranda/magicsquare/IndividualTest.scala
@@ -6,6 +6,7 @@ import scala.util.Random
 class IndividualTest
     extends wordspec.AnyWordSpec
     with matchers.should.Matchers {
+
   "A new Individal " when {
     "chromosome size is 1" should {
       "be Individual.chromosome == Seq(0)" in {
@@ -42,41 +43,84 @@ class IndividualTest
       }
     }
 
-    // "chromosome is given" should {
-    //   "be Individual.chromosome == Seq(0,1,2,3,4,5)" in {
-    //     Individual(Seq(0, 1, 2, 3, 4, 5)).chromosome shouldBe Seq(0, 1, 2, 3, 4,
-    //       5)
-    //   }
-    // }
+    "chromosome size is 4 and Random seed 0" should {
+      "be Individual.chromosome == Seq(3, 0, 1, 2)" in {
+        val actual: Option[Individual] = Individual(
+          chromosomeSize = 4,
+          randomGenerator = new Random(0L)
+        )
 
-    //   "modify chromosome Seq(0,1,2) from 1 -> 9" should {
-    //     "be Seq(0,9,2)" in {
-    //       Individual(Seq(0,1,2)).modify(pos = 1, gene = 9).chromosome shouldBe Seq(0,9,2)
-    //     }
-    //   }
-    // }
+        val expected: Option[Chromosome] = Chromosome(Seq(3, 0, 1, 2))
 
-    // "An existing Individal " when {
-    //   "whith chromosome Seq(1,2,3,4)" should {
-    //     "be Individual.fitness == Some(33)" in {
-    //       Individual(Seq(1,2,3,4)).calcFitness shouldBe 33
-    //     }
-    //   }
+        actual.get.chromosome shouldBe expected.get
+      }
+    }
 
-    //   "whith chromosome Seq(1,2,3,4,5,6,7,8,9,10) and mutationRate = 0.0" should {
-    //     "have no mutation" in {
-    //       Individual(Seq(1,2,3,4,5,6,7,8,9,10)).mutation(0.0,new Random(0)).chromosome should contain theSameElementsInOrderAs(Vector(1,2,3,4,5,6,7,8,9,10))
-    //     }
-    //   }
+    "chromosome size is 9 and Random seed 0" should {
+      "be Individual.chromosome == Seq(3, 7, 2, 1, 0, 5, 4, 8, 6)" in {
+        val actual: Option[Individual] = Individual(
+          chromosomeSize = 9,
+          randomGenerator = new Random(0L)
+        )
 
-    //   "whith chromosome Seq(1,2,3,4,5,6,7,8,9,10) and mutationRate = 1.0" should {
-    //     "have mutation form Seq(1,2,3,4,5,6,7,8,9,10) to Seq(6,10,8,3,4,9,2,7,1,5)" in {
-    //       Individual(Seq(1,2,3,4,5,6,7,8,9,10)).mutation(1.0,new Random(0)).chromosome should contain theSameElementsInOrderAs(Vector(1,2,5,4,3,6,7,8,9,10))
-    //     }
-    //   }
-    // }
+        val expected: Option[Chromosome] =
+          Chromosome(Seq(3, 7, 2, 1, 0, 5, 4, 8, 6))
 
-    // "Two existing Individals" when {
+        actual.get.chromosome shouldBe expected.get
+      }
+    }
+
+    "chromosome is given" should {
+      "be Individual.fitness == Seq(3, 7, 2, 1, 0, 5, 4, 8, 6)" in {
+        val c: Chromosome = Chromosome(Seq(3, 7, 2, 1, 0, 5, 4, 8, 6)).get
+
+        val actual: Option[Individual] = Individual(chromosome = c)
+
+        val expected: Option[Long] =
+          MagicSquare.squareDiferences(chromosome = c)
+
+        actual.get.fitness shouldBe expected.get
+      }
+    }
+  }
+
+  "An existing Individal " when {
+    "whith chromosome Seq(1,2,3,4)" should {
+      "be Individual.fitness == Some(33)" in {
+        val c: Chromosome = Chromosome(Seq(1, 2, 3, 4)).get
+
+        val actual: Option[Individual] = Individual(chromosome = c)
+
+        actual.get.fitness shouldBe 33
+      }
+    }
+
+    "whith chromosome Seq(1,2,3,4,5,6,7,8,9) and mutationRate = 0.0" should {
+      "have no mutation" in {
+        val c: Chromosome = Chromosome(Seq(1, 2, 3, 4, 5, 6, 7, 8, 9)).get
+
+        val actual: Individual = Individual(chromosome = c).get
+          .mutation(mutationRate = 0.0, randomGenerator = new Random(0L))
+
+        actual.chromosome.value should contain theSameElementsInOrderAs (
+            Vector(1, 2, 3, 4, 5, 6, 7, 8, 9))
+      }
+    }
+
+      "whith chromosome Seq(1,2,3,4,5,6,7,8,9) and mutationRate = 1.0" should {
+        "have mutation form Seq(1,2,3,4,5,6,7,8,9) to Seq(6,8,3,4,9,2,7,1,5)" in {
+        val c: Chromosome = Chromosome(Seq(1, 2, 3, 4, 5, 6, 7, 8, 9)).get
+
+        val actual: Individual = Individual(chromosome = c).get
+          .mutation(mutationRate = 1.0, randomGenerator = new Random(0L))
+
+        actual.chromosome.value should contain theSameElementsInOrderAs (
+            Vector(1, 5, 3, 4, 2, 6, 7, 8, 9))
+        }
+      }
+    }
+
+    "Two existing Individals" when {
     //   "whith chromosomes Seq(1,2,3,4) and Seq(1,2,3,4) and crossover = 0.0" should {
     //     "produce Seq(1,2,3,4)" in {
     //       val actual: Seq[Long] =
