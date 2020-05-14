be790056ce2ffc0a8636d4964f35203ad9e1018c
Ricardo Miranda <mail@ricardoMiranda>
2020-05-15 00:52:38 +0100

    started to uncomment main

--- src/main/scala/com/ricardomiranda/magicsquare/Main.scala
+++ src/main/scala/com/ricardomiranda/magicsquare/Main.scala
@@ -22,6 +22,9 @@ case class Result(
 
 object Main extends App with StrictLogging {
 
+  // Start tmmer to check running time
+  val t0 = System.nanoTime()
+
   val arguments: ArgumentParser =
     ArgumentParser.parser.parse(args, ArgumentParser()) match {
       case Some(config) => config
@@ -30,40 +33,40 @@ object Main extends App with StrictLogging {
 
   logger.info("All input arguments were correctly parsed")
 
-  // val sparkSession: SparkSession = Computation.createSparkSession(
-  //   configs = arguments.configs,
-  //   sparkAppName = arguments.sparkAppName
+  val sparkSession: SparkSession = Computation.createSparkSession(
+    configs = arguments.configs,
+    sparkAppName = arguments.sparkAppName
+  )
+
+  sparkSession.sparkContext.setLogLevel("INFO")
+
+  val magicSquareConfigurationFilePath: String =
+    Computation.addFileToSparkContext(
+      filePath = arguments.magicSquareConfigurationFile,
+      sparkSession = sparkSession
+    )
+
+  val magicSquareConfigs: MagicSquareJsonSupport.MagicSquareConfiguration =
+    MagicSquareJsonSupport
+      .magicSquareConfigFileContentsToObject(configurationFilePath =
+        magicSquareConfigurationFilePath
+      )
+
+  val iniPopulation =
+    Population(
+      chromosomeSize =
+        (magicSquareConfigs.sideSize * magicSquareConfigs.sideSize).toLong,
+      populationSize = magicSquareConfigs.popSize,
+      sparkSession = sparkSession
+    )
+
+  // logger.info("Initial solution is:")
+  // logger.info(
+  //   MagicSquare(iniPopulation).m.toString
   // )
-
-  // sparkSession.sparkContext.setLogLevel("INFO")
-
-  // val magicSquareConfigurationFilePath: String =
-  //   Computation.addFileToSparkContext(
-  //     filePath = arguments.magicSquareConfigurationFile,
-  //     sparkSession = sparkSession
-  //   )
-
-  // val magicSquareConfigs: MagicSquareJsonSupport.MagicSquareConfiguration =
-  //   MagicSquareJsonSupport
-  //     .magicSquareConfigFileContentsToObject(configurationFilePath =
-  //       magicSquareConfigurationFilePath
-  //     )
-
-  // val iniPopulation =
-  //   Population(
-  //     chromosomeSize =
-  //       (magicSquareConfigs.sideSize * magicSquareConfigs.sideSize).toLong,
-  //     populationSize = magicSquareConfigs.popSize,
-  //     sparkSession = sparkSession
-  //   )
-
-//   logger.info("Initial solution is:")
-//   logger.info(
-//     MagicSquare(iniPopulation).m.toString
-//   )
-//   logger.info(
-//     s"With fitness: ${iniPopulation.individuals.sortByKey().first._1}"
-//   )
+  // logger.info(
+  //   s"With fitness: ${iniPopulation.individuals.sortByKey().first._1}"
+  // )
 
 //   val result: Seq[Result] = Computation.loop(
 //     0,
@@ -78,94 +81,93 @@ object Main extends App with StrictLogging {
 //     case _                =>
 //   }
 
-//   Computation.finalOutput(
-//     result = result,
-//     sideSize = magicSquareConfigs.sideSize
-//   )
+  // Computation.finalOutput(
+  //   result = result,
+  //   sideSize = magicSquareConfigs.sideSize
+  // )
 
-//   logger.info("Program terminated")
-//   val t0 = System.nanoTime()
-//   val t1 = System.nanoTime()
-//   logger.info("Elapsed time: " + (t1 - t0) + "ns")
+  logger.info("Program terminated")
+  val t1 = System.nanoTime()
+  logger.info("Elapsed time: " + (t1 - t0) + "ns")
 
-//   sparkSession.stop()
-// }
+  sparkSession.stop()
+}
 
-// case object Computation extends StrictLogging {
+case object Computation extends StrictLogging {
 
-//   /**
-//     * Method to add a file to the Spark session.
-//     *
-//     * @param filePath     The filepath to the file to add.
-//     * @param sparkSession The Spark session of the application.
-//     * @return The file path of the file in the Spark session.
-//     */
-//   def addFileToSparkContext(
-//       filePath: String,
-//       sparkSession: SparkSession
-//   ): String = {
-//     logger.info(s"Add file ${filePath} to Spark Session")
+  /**
+    * Method to add a file to the Spark session.
+    *
+    * @param filePath     The filepath to the file to add.
+    * @param sparkSession The Spark session of the application.
+    * @return The file path of the file in the Spark session.
+    */
+  def addFileToSparkContext(
+      filePath: String,
+      sparkSession: SparkSession
+  ): String = {
+    logger.info(s"Add file ${filePath} to Spark Session")
 
-//     sparkSession.sparkContext.addFile(filePath)
-//     val filePathBasename: String =
-//       Seq(
-//         FilenameUtils.getBaseName(filePath),
-//         FilenameUtils.getExtension(filePath)
-//       ).mkString(".")
+    sparkSession.sparkContext.addFile(filePath)
+    val filePathBasename: String =
+      Seq(
+        FilenameUtils.getBaseName(filePath),
+        FilenameUtils.getExtension(filePath)
+      ).mkString(".")
 
-//     SparkFiles.get(filePathBasename)
-//   }
+    SparkFiles.get(filePathBasename)
+  }
 
-//   /**
-//     * Creates a spark session using the provided configurations.
-//     * Should the configurations be empty, the default spark session will be returned.
-//     *
-//     * @param configs provided configurations.
-//     * @return a configured spark session.
-//     */
-//   def createSparkSession(
-//       configs: Map[String, String],
-//       sparkAppName: String
-//   ): SparkSession = {
-//     logger.info(s"Creating Spark Session with name $sparkAppName")
+  /**
+    * Creates a spark session using the provided configurations.
+    * Should the configurations be empty, the default spark session will be returned.
+    *
+    * @param configs provided configurations.
+    * @return a configured spark session.
+    */
+  def createSparkSession(
+      configs: Map[String, String],
+      sparkAppName: String
+  ): SparkSession = {
+    logger.info(s"Creating Spark Session with name $sparkAppName")
 
-//     val builder =
-//       SparkSession
-//         .builder()
-//         .appName(sparkAppName)
+    val builder =
+      SparkSession
+        .builder()
+        .appName(sparkAppName)
 
-//     Some(configs.foldLeft(builder)((accum, x) => accum.config(x._1, x._2)))
-//       .getOrElse(builder)
-//       .getOrCreate()
-//   }
+    Some(configs.foldLeft(builder)((accum, x) => accum.config(x._1, x._2)))
+      .getOrElse(builder)
+      .getOrCreate()
+  }
 
-//   /**
-//     * Print final program results
-//     *
-//     * @param result   Sequence o Results stroed during the computation
-//     * @param sideSize Magic square side size
-//     */
-//   def finalOutput(result: Seq[Result], sideSize: Int): Unit = {
-//     logger.info(s"With fitness: ${result.head.fitness}")
+  /**
+    * Print final program results
+    *
+    * @param result   Sequence o Results stroed during the computation
+    * @param sideSize Magic square side size
+    */
+  def finalOutput(result: Seq[Result], sideSize: Int): Unit = {
+    logger.info(s"With fitness: ${result.head.fitness}")
 
-//     val fig = Figure()
-//     val plt = fig.subplot(0)
-//     plt += plot(
-//       result.map(x => x.lineNbr),
-//       result.map(x => x.fitness),
-//       name = "Best individual"
-//     )
-//     plt += plot(
-//       result.map(x => x.lineNbr),
-//       result.map(x => x.newGenerationPopulationFitness.toInt),
-//       name = "Population"
-//     )
-//     plt.xlabel = "Iterations"
-//     plt.ylabel = "Fitness"
-//     plt.title = s"Magic square with size ${sideSize} fitness"
-//     plt.legend = true
-//     fig.refresh()
-//   }
+    val fig = Figure()
+    val plt = fig.subplot(0)
+    plt += plot(
+      result.map(x => x.lineNbr),
+      result.map(x => x.fitness),
+      name = "Best individual"
+    )
+    plt += plot(
+      result.map(x => x.lineNbr),
+      result.map(x => x.newGenerationPopulationFitness.toInt),
+      name = "Population"
+    )
+    plt.xlabel = "Iterations"
+    plt.ylabel = "Fitness"
+    plt.title = s"Magic square with size ${sideSize} fitness"
+    plt.legend = true
+    fig.refresh()
+  }
 
 //   /**
 //     * This the main program loop
